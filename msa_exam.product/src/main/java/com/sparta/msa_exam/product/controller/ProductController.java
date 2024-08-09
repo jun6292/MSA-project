e7337ade5a21ccfa6590664f32b4880c0fc5e793
jun6292 <c68254@gmail.com>
2024-08-09 15:04:34 +0900

    Feat: 상품 추가 API, 상품 목록 조회 API 구현

--- msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
+++ msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
@@ -1,13 +1,12 @@
 package com.sparta.msa_exam.product.controller;
 
+import com.sparta.msa_exam.product.common.response.CommonResponse;
+import com.sparta.msa_exam.product.dto.ProductRequestDto;
 import com.sparta.msa_exam.product.service.ProductService;
 import com.sparta.msa_exam.product.dto.ProductResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,21 +16,14 @@ import java.util.List;
 public class ProductController {
     private final ProductService productService;
 
-    /**
-     * 상품 등록 API
-     * @return
-     */
     @PostMapping("")
-    public String createProduct() {
-        return "상품이 등록되었습니다.";
+    public CommonResponse<?> createProduct(@RequestBody ProductRequestDto productRequestDto) {
+        productService.createProduct(productRequestDto);
+        return CommonResponse.created("상품 등록 완료");
     }
 
-    /**
-        * 상품 목록 조회 API
-     * @return 상품 목록
-     */
     @GetMapping("")
-    public ResponseEntity<List<ProductResponseDto>> readProducts() {
-        return ResponseEntity.ok(productService.readProducts());
+    public CommonResponse<List<ProductResponseDto>> readProducts() {
+        return CommonResponse.ok(productService.readProducts());
     }
 }
--- msa_exam.product/src/main/java/com/sparta/msa_exam/product/service/ProductService.java
+++ msa_exam.product/src/main/java/com/sparta/msa_exam/product/service/ProductService.java
@@ -1,21 +1,41 @@
 package com.sparta.msa_exam.product.service;
 
+import com.sparta.msa_exam.product.dto.ProductRequestDto;
+import com.sparta.msa_exam.product.entity.Product;
 import com.sparta.msa_exam.product.repository.ProductRepository;
 import com.sparta.msa_exam.product.dto.ProductResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
+    @Transactional(readOnly = true)
     public List<ProductResponseDto> readProducts() {
-        return List.of(
-                new ProductResponseDto(1L, "맥북", 2000000),
-                new ProductResponseDto(2L, "아이패드", 1000000)
-        );
+        List<Product> productList = productRepository.findAll();
+        List<ProductResponseDto> productResponseDtoList = productList.stream()
+                .map(product -> ProductResponseDto.builder()
+                        .productId(product.getId())
+                        .name(product.getName())
+                        .supplyPrice(product.getSupplyPrice())
+                        .build())
+                .collect(Collectors.toList());
+        return productResponseDtoList;
+    }
+
+    @Transactional
+    public void createProduct(ProductRequestDto productRequestDto) {
+        Product product = Product.builder()
+                .name(productRequestDto.name())
+                .supplyPrice(productRequestDto.supplyPrice())
+                .build();
+        productRepository.save(product);
     }
 }
