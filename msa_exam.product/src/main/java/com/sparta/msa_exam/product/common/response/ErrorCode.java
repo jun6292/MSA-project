7cae1cc23a56ada8f99dffce9e7ee739117fcb36
jun6292 <c68254@gmail.com>
2024-08-10 08:28:36 +0900

    Fix: 상품 에러코드 수정 - p(roduct) 추가

--- msa_exam.product/src/main/java/com/sparta/msa_exam/product/common/response/ErrorCode.java
+++ msa_exam.product/src/main/java/com/sparta/msa_exam/product/common/response/ErrorCode.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     // 400
-    INVALID_PARAMETER("40000", HttpStatus.BAD_REQUEST, "유효하지 않는 파라미터입니다."),
-    MISSING_REQUEST_PARAMETER("40001", HttpStatus.BAD_REQUEST, "필수 파라미터가 누락되었습니다."),
-    MISSING_REQUEST_BODY("40002", HttpStatus.BAD_REQUEST, "요청 바디가 누락되었습니다."),
+    INVALID_PARAMETER("p40000", HttpStatus.BAD_REQUEST, "유효하지 않는 파라미터입니다."),
+    MISSING_REQUEST_PARAMETER("p40001", HttpStatus.BAD_REQUEST, "필수 파라미터가 누락되었습니다."),
+    MISSING_REQUEST_BODY("p40002", HttpStatus.BAD_REQUEST, "요청 바디가 누락되었습니다."),
 
     // 401
 
@@ -18,11 +18,11 @@ public enum ErrorCode {
     // 403
 
     // 404
-    NOT_FOUND_END_POINT("40400", HttpStatus.NOT_FOUND, "존재하지 않는 엔드포인트입니다."),
-    PRODUCT_NOT_FOUND("40401", HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
+    NOT_FOUND_END_POINT("p40400", HttpStatus.NOT_FOUND, "존재하지 않는 엔드포인트입니다."),
+    PRODUCT_NOT_FOUND("p40401", HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
 
     // 500
-    SERVER_ERROR("50000", HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+    SERVER_ERROR("p50000", HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
