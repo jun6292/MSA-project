1598119efc36ed060a1ccadcd9a2c46ff8f0ad13
jun6292 <c68254@gmail.com>
2024-08-13 12:05:22 +0900

    Feat: 회원가입한 유저 검증 기능 구현

---
+++ msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/AuthFeignClient.java
@@ -0,0 +1,11 @@
+package com.sparta.msa_exam.gateway;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "auth-service")
+public interface AuthFeignClient extends AuthService {  // 인증 서비스 호출 클라이언트가 응용 계층의 인터페이스인 AuthService 를 상속받아 DIP 를 적용
+    @GetMapping("/auth/verify")
+    Boolean verifyUser(@RequestParam("user_id") String userId);
+}
---
+++ msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/AuthService.java
@@ -0,0 +1,5 @@
+package com.sparta.msa_exam.gateway;
+
+public interface AuthService {
+    Boolean verifyUser(String userId);
+}
